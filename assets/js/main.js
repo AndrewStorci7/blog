609c32e75d3330251c02017799e92fdd23cb8986
Andrea Storci <AndrewStorci7>
2023-11-18 17:30:38 +0100

    Updated header & added news-section - Header: - - Fixed issue with dropdown - - Removed icons - News Section: - - Added spotify playlist section

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+jQuery('window').ready( ($) => {
+    const dropdown_appunti = $('#dropdown-appunti');
+    const navbar_header = $('#navbar-header');
+    let open_appunti = false;
+
+    /*
+    dropdown_appunti.click( () => {
+        navbar_header.addClass('dropdown-effect-appunti');
+        open_appunti = true;
+    });
+    $(document).click( (e) => {
+        if (!$(e.target).closest('#navbar_header').length && open_appunti) {
+            navbar_header.removeClass('dropdown-effect-appunti');
+            open_appunti = false;
+        }
+    });
+    */
+});
